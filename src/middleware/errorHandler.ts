202220e44deda6a008a3e11d72d733c4364bde56
winoooops <winoooops@outlook.com>
2024-06-25 15:27:41 +0800

    REF: update error handling to include AxiosError

--- src/middleware/errorHandler.ts
+++ src/middleware/errorHandler.ts
@@ -1,15 +1,27 @@
 import {NextFunction, Request, Response} from "express"
 import {CMCError} from "../utils/CMCError";
-import {CMCErrorResponse} from "../types/CMC";
+import {AxiosError} from "axios";
 
 export function errorHandlerMiddleware(error: unknown, req: Request, res: Response, next: NextFunction) {
   // if the header is already sent, pass to the next error handler
   if(res.headersSent) return next(error);
 
   if(error instanceof CMCError) {
-    const cmcError = error as CMCErrorResponse;
+    const cmcError = error as CMCError;
     res.status(cmcError.status.error_code).json({...cmcError.status});
-  } else {
+  }
+  else if (error instanceof AxiosError) {
+    const axiosError = error as AxiosError;
+    if(axiosError.response) {
+      res.status(axiosError.response.status).json(axiosError.response.data);
+    } else {
+      res.status(500).json({
+        error_code: 500,
+        error_message: axiosError.message || "Internal Server Error"
+      });
+    }
+  }
+  else {
     const knownError = error as Error;
     res.status(500).json({
       error_code: 500,
